49fb33009523a5cfdfed3ce5e5e7636b14f8fa8d
Rafael Suzano <rafaelsuzano@hotmail.com>
2021-01-14 21:33:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,63 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const mysql = require('mysql');
+var http = require('http');
 
-
-
-var connection = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:'12345678',
-    database: "cotacao"
-});
-
-
-
-connection.connect();
-const dados = {nome:"Rafael", idade:35}
-const dados1 = {nome:"Suzano", idade:35}
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-
-app.get('/api/v1', (req, res) => {
-    res.json(
-        { dados}
-    )
-  }
-     )
-
-
-app.get('/api/v2', (req, res) => {
-res.json(
-{ dados1}
-        )
-      }
-         )     
-
-
-
-
-app.get('/api/v3', (req, res) => {
-res.json(
-{ dados1}
-        )
-      }
-
-      )     
-
-app.get('/api/cotacao', (req, res) => {
-    connection.query(' select * from cotacao_full_dolar', function(err, rows, fields) {
-        if (err) throw err;
-        res.json({cotacao:rows});
-
-      });
-      
-      
-
-}
-                 ) ;        
-            
+var config = {
+    hostname: 'localhost',
+    port: 3000,
+    method: 'post',
+    path: '/produtos',
+    headers: {
+        'accept': 'application/json'
+    }
+};
